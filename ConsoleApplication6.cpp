32f87aff88a6ef52d72ea2433c03a3fa6e3d6928
Gleb Kayumov <glebaskayumov@yandex.ru>
2025-03-03 12:05:17 +0200

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -1,45 +1,28 @@
-﻿#include <iostream> 
+#include <iostream> 
 
-using namespace std;
-
-class MyVector
-{
-private:
+class MyVector {
     int _size;
     int _capacity;
     int* _data;
-
 public:
-    int size()
-    {
-        return _size;
-    }
+    int size() {return _size;}
 
-    int capacity()
-    {
-        return _capacity;
-    }
+    int capacity(){return _capacity;}
 
-    int& operator [] (int i) const
-    {
-        return _data[i];
-    }
+    int& operator [] (int i) const {return _data[i];}
 
-    void setValue(int i, int x)
-    {
-        if (i >= 0 && i < _size && x >= 0)
+    void setValue(int i, int x) {
+        if (i >= 0 && i < _size)
             _data[i] = x;
     }
 
-    MyVector()
-    {
+    MyVector() {
         _size = 0;
         _capacity = 8;
         _data = new int[8];
     }
 
-    MyVector(const MyVector& a)
-    {
+    MyVector(const MyVector& a) {
         _size = a._size;
         _capacity = a._capacity;
         _data = new int[_capacity];
@@ -47,51 +30,42 @@ public:
             _data[i] = a[i];
     }
 
-    MyVector(int n, int initialValue)
-    {
+    MyVector(int n, int initialValue) {
         _capacity = 2 * n;
         _size = n;
-
         _data = new int[2 * n];
         for (int i = 0; i < n; i++)
             _data[i] = initialValue;
     }
 
-    void print()
-    {
+    void print() {
         for (int i = 0; i < _size; i++)
-            cout << _data[i] << " ";
-        cout << endl;
+            std::cout << _data[i] << " ";
+        std::cout << std::endl;
     }
 
-    MyVector& operator = (const MyVector& a)
-    {
+    MyVector& operator = (const MyVector& a) {
         _size = a._size;
         _capacity = a._capacity;
         _data = new int[_capacity];
         for (int i = 0; i < _size; i++)
             _data[i] = a[i];
-
         return *this;
     }
 
-    ~MyVector()
-    {
+    ~MyVector() {
         delete[] _data;
     }
 
-    int front()
-    {
+    int front() {
         return _data[0];
     }
 
-    int back()
-    {
+    int back() {
         return _data[_size - 1];
     }
 
-    void push_back(int x)
-    {
+    void push_back(int x) {
         if (_size < _capacity) {
             _data[_size++] = x;
         }
@@ -106,28 +80,8 @@ public:
             _size++;
         }
     }
-    /*MyVector operator + (const MyVector& v)
-    {
-        MyVector result;
-
-        if (_size == v._size) {
-            for (int i = 0; i < _size; i++)
-                result.push_back(_data[i] + v._data[i]);
-        }
-
-        if (_size == v._size) {
-            result._data = new int[2 * _size];
-            result._capacity = 2 * _size;
-            result._size = _size;
-            for (int i = 0; i < _size; i++)
-                result._data[i] = _data[i] + v._data[i];
-        }
-
-        return result;
-    }*/
-
-    friend MyVector operator + (const MyVector& a, const MyVector& b)
-    {
+    
+    friend MyVector operator + (const MyVector& a, const MyVector& b) {
         MyVector result;
 
         if (a._size == b._size) {
@@ -137,8 +91,7 @@ public:
         return result;
     }
 
-    friend MyVector operator - (const MyVector& a, const MyVector& b)
-    {
+    friend MyVector operator - (const MyVector& a, const MyVector& b) {
         MyVector result;
 
         if (a._size == b._size) {
@@ -148,8 +101,7 @@ public:
         return result;
     }
 
-    friend int operator * (const MyVector& a, const MyVector& b)
-    {
+    friend int operator * (const MyVector& a, const MyVector& b) {
         int result = 0;
 
         if (a._size == b._size) {
@@ -159,8 +111,7 @@ public:
         return result;
     }
 
-    friend MyVector operator * (const MyVector& a, int k)
-    {
+    friend MyVector operator * (const MyVector& a, int k) {
         MyVector result;
 
         for (int i = 0; i < a._size; i++)
@@ -169,39 +120,38 @@ public:
     }
     
 };
-    int main() 
-    {
-        int p = 5;
 
-        MyVector a(5, 29); 
-        a.print();
-        MyVector b(5, 27);
-        b.print();
-        MyVector c = a + b;
-        cout << "Sum: " << endl;
-        c.print();
+int main() {
+    int p = 5;
+    MyVector a(5, 1); 
+    a.print();
+    
+    MyVector b(5, 2);
+    b.print();
+    
+    MyVector c = a + b;
+    std::cout << "Sum: " << std::endl;
+    c.print();
 
-        MyVector c1 = a - b;
-        cout << "Difference: " << endl; c1.print();
+    MyVector c1 = a - b;
+    std::cout << "Difference: " << std::endl; 
+    c1.print();
 
-        int d  = a * b;
-        cout << d << endl;
+    int d  = a * b;
+    std::cout << d << std::endl;
+
+    MyVector c2 = a * p;
+    c2.print();
         
 
-        MyVector c2 = a * p;
-        c2.print();
-        
 
+    std::cout << a[3] << std::endl; 
+    a.setValue(3, 50); 
+    a.setValue(3, 100); 
+    a.setValue(1, 200); 
 
-        cout << a[3] << endl; 
-        a.setValue(3, 50); 
-        a.setValue(3, 100); 
-        a.setValue(1, 200); 
-
-        b.setValue(1, 777); 
-
-        cout << a.front(); 
-
-        return 0; 
-    }
-
+    b.setValue(1, 777); 
+    std::cout << a.front(); 
+    
+    return 0; 
+}
